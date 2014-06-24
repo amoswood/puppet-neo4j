0597b8fbaa3be9c61bdb1d49d08437c0fda88531
Amos Wood <amos.wood@lifeway.com>
2014-06-24 10:57:50 -0500

    removed unused files

--- tests/tarball.pp
+++
@@ -1,5 +0,0 @@
-lw_neo4j::tarball { 'community' :
-    pkg_tgz => 'neo4j-community-2.1.2.tgz',
-    download_path => 'http://download.neo4j.org/artifact?edition=community&version=2.1.2&distribution=tarball',
-    install_dir => '/opt/neo4j',
-}
