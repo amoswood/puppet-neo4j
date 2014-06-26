39b3296c6b1a1c9a6d662bafc38653fb57daa26f
Amos Wood <amos.wood@lifeway.com>
2014-06-26 17:16:34 -0500

    fixed style issues
    Former-commit-id: 9f86e05912e02f689ab6103ba967bfc9cd22f896

--- tests/init.pp
+++ tests/init.pp
@@ -14,15 +14,15 @@ $auth_users = {
 }
 
 class { 'neo4j' :
-  version => '2.1.2',
-  edition => 'enterprise',
-  install_prefix => '/opt/neo4j',
-  jvm_init_memory => '1024',
-  jvm_max_memory => '1024',
+  version                  => '2.1.2',
+  edition                  => 'enterprise',
+  install_prefix           => '/opt/neo4j',
+  jvm_init_memory          => '1024',
+  jvm_max_memory           => '1024',
   allow_remote_connections => true,
-  auth_ensure => present,
-  auth_admin_user => 'admin',
-  auth_admin_password => 'password'
-  auth_users => $auth_users
-  newrelic_ensure => absent,
+  auth_ensure              => present,
+  auth_admin_user          => 'admin',
+  auth_admin_password      => 'password',
+  auth_users               => $auth_users,
+  newrelic_ensure          => absent,
 }
