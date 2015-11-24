641367c5633e4917322793f9d014a98ad9c6603d
Jeffrey Meyers <meyers2@pdx.edu>
2015-11-24 09:02:12 -0800

    clarify ipaddress is a top-scope variable

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,7 @@ class neo4j (
 
   #server options
   $allow_remote_connections = true,
-  $address = $ipaddress,
+  $address = $::ipaddress,
   $jvm_init_memory = '1024',
   $jvm_max_memory = '1024',
 
